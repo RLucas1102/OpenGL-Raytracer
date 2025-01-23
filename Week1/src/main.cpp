7c0640892ce8ca1c5f3b937d2ffeace9ad3641aa
RLucas1102 <robenoltl@gmail.com>
2025-01-22 23:26:09 -0500

    Created another cube and plane

--- Week1/src/main.cpp
+++ Week1/src/main.cpp
@@ -62,7 +62,7 @@ int main() {
 
     // Vertex/Index data
 	// -----------------
-	float vertices[] = {
+	float cubeVertices[] = {
 		// Vertices			// Colors
 		0.5f,  0.5f,  0.5f,	 1.0f, 0.0f, 0.0f,
 		0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
@@ -74,7 +74,7 @@ int main() {
 	   -0.5f, -0.5f, -0.5f,	 1.0f, 0.5f, 0.5f
 	};
 
-	unsigned int indices[] = {
+	unsigned int cubeIndices[] = {
 		7, 3, 1,
 		1, 5, 7,
 
@@ -95,20 +95,33 @@ int main() {
 
 	};
 
-	// Bind and load buffers with vertex/index data
-	// --------------------------------------------
-	unsigned int VBO, VAO, EBO; // Declare buffers
-	glGenBuffers(1, &VBO);		// Generate IDs for buffers
-	glGenBuffers(1, &EBO);
-	glGenVertexArrays(1, &VAO);
+    float planeVertices[] = {
+        // Vertices             // Colors
+        -0.5f, -0.5f, 0.0f,     0.5f, 0.5f, 0.5f,
+         0.5f, -0.5f, 0.0f,     0.5f, 0.5f, 0.5f,
+         0.5f,  0.5f, 0.0f,     0.5f, 0.5f, 0.5f,
+        -0.5f,  0.5f, 0.0f,     0.5f, 0.5f, 0.5f 
+    };
 
-	glBindVertexArray(VAO); // Bind the vertex array first
+    unsigned int planeIndices[] = {
+        0,1,2,
+        2,3,0
+    }; 
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO); // Bind as an array buffer for vertices
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Load vertex data into VBO
+	// Bind and load buffers with vertex/index data for cube
+	// -----------------------------------------------------
+	unsigned int cubeVBO, cubeVAO, cubeEBO; // Declare buffers
+	glGenBuffers(1, &cubeVBO);		// Generate IDs for buffers
+	glGenBuffers(1, &cubeEBO);
+	glGenVertexArrays(1, &cubeVAO);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // Bind buffers as element array buffer
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // Load index data in EBO
+	glBindVertexArray(cubeVAO); // Bind the vertex array first
+
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO); // Bind as an array buffer for vertices
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW); // Load vertex data into VBO
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeEBO); // Bind buffers as element array buffer
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW); // Load index data in EBO
 
 	// Position data
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); // Locate the vertex position attribute on GPU
@@ -118,25 +131,58 @@ int main() {
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); // Locate the color attribute on GPU
 	glEnableVertexAttribArray(1);
 
+    // Bind and load buffers with vertex/index data for plane
+    // ------------------------------------------------------
+    unsigned int planeVBO, planeVAO, planeEBO; // Declare buffers
+    glGenBuffers(1, &planeVBO); // Generate IDs for buffers
+    glGenBuffers(1, &planeEBO);
+    glGenVertexArrays(1, &planeVAO);
+
+    glBindVertexArray(planeVAO); // Bind vertex array first
+
+    glBindBuffer(GL_ARRAY_BUFFER, planeVBO); // Bind as array buffer for vertices
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW); // Load in vertex data into VBO
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, planeEBO); // Bind as element array buffer for indices
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(planeIndices), planeIndices, GL_STATIC_DRAW); // Load in index data into EBO
+
+    // Position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); // Locate position attribute on GPU
+    glEnableVertexAttribArray(0); // Enable position
+
+    // Color 
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); // Locate color attribute on GPU
+    glEnableVertexAttribArray(1); // Enable Color
+
     // Camera settings
     // ---------------
-    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-    glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraPos = glm::vec3(0.5f, 1.0f, 3.0f);
+    glm::vec3 cameraFront = glm::vec3(0.0f, -0.2f, -1.0f);
     glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
     // Object settings
-    // -------------
-    glm::vec3 cube1 = glm::vec3(0.0f,  0.0f,  0.0f);
+    // ---------------
+    glm::vec3 cube1 = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 cube2 = glm::vec3(1.0f, 0.0f, -5.0f);
+    glm::vec3 plane = glm::vec3(0.0f, -0.5f, -2.0f);
+
+    // Depth settings (Configuring Global OpenGL state)
+    // ------------------------------------------------
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_TRUE); // Depth buffer enabled
+    glDepthFunc(GL_LESS); // Appears as if we never enabled depth testing;
 
     // Render Loop
     // -----------
     while(!glfwWindowShouldClose(myWindow)) {
         
         // Rendering commands
-        glClearColor(0.0f, 1.0f, 0.4f, 1.0f); // state-setting
-        glClear(GL_COLOR_BUFFER_BIT); // state-using
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // state-setting
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // state-using
 
         ourShader.use(); // Activate our shader
+
+        glBindVertexArray(cubeVAO);
 
         // Projection matrix
 	    // Our projection rarely changes, thus it can be kept outside the main render loop and set only once
@@ -147,13 +193,37 @@ int main() {
         glm::mat4 view;
 		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
-        // Model matrix
+        // Model matrix (cube1)
 		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, cube1);
+        model = glm::translate(model, cube1);
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
         // Pass transformations
         ourShader.setMat4("projection", projection);
 		ourShader.setMat4("view", view);
+        ourShader.setMat4("model", model);
+
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0); // Draw the 36 vertices (1 cube, 6 faces, 12 triangles)
+
+        // Model matrix (cube2)
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, cube2);
+        model = glm::rotate(model, glm::radians(75.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+        // Pass transformations
+        ourShader.setMat4("model", model);
+
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0); // Draw the 36 vertices (1 cube, 6 faces, 12 triangles)
+
+        glBindVertexArray(planeVAO);
+
+        // Model matrix (plane)
+        model = model = glm::mat4(1.0f);
+        model = glm::translate(model, plane);
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(10.0f));
+
+        // Pass transformations
         ourShader.setMat4("model", model);
 
         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0); // Draw the 36 vertices (1 cube, 6 faces, 12 triangles)
@@ -166,9 +236,12 @@ int main() {
 
     // Clean up
     // --------
-    glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &VAO);
-	glDeleteBuffers(1, &EBO);
+    glDeleteBuffers(1, &cubeVBO);
+	glDeleteBuffers(1, &cubeVAO);
+	glDeleteBuffers(1, &cubeEBO);
+    glDeleteBuffers(1, &planeVBO);
+	glDeleteBuffers(1, &planeVAO);
+	glDeleteBuffers(1, &planeEBO);
     glfwTerminate();
 
     return 0;
