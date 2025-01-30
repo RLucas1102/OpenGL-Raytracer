b3df98b239d961dd2d1e1c5918a6c93ec3dca795
RLucas1102 <robenoltl@gmail.com>
2025-01-30 10:03:45 -0500

    Code generates a ppm file from pixel RGB values

---
+++ Week2/src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+
+    // File
+    std::ofstream myFile;
+    myFile.open("image.ppm");
+
+    // Image
+
+    int img_width  = 256;
+    int img_height = 256;
+
+    // Render
+
+    myFile << "P3\n" << img_width << ' ' << img_height << "\n255\n";
+
+    for (int i = 0; i < img_height; i++) {
+        for (int j = 0; j < img_width; j++) {
+            auto r = double(j) / (img_width-1);
+            auto g = double(i) / (img_height-1);
+            auto b = double(i) / (img_height-1);
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            myFile << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    myFile.close();
+
+    return 0;
+}
