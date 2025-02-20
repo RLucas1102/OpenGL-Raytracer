0e6d122d696b0339dcea8a57c7ea0a7a809ed3b5
RLucas1102 <robenoltl@gmail.com>
2025-02-20 00:23:50 -0500

    Added good bit; Determines img size from width and aspect; camera/viewport setup; rays shoot through each pixel and determine color; created gradient

--- Week5/src/main.cpp
+++ Week5/src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <vec/vec3.h>
+#include <vec/ray.h>
 #include <fstream>
 
 // Prototypes
 // ----------
 void writeColor(std::ofstream& outputFile, const vec3& pixel_color);
+
+vec3 ray_color(const ray& r);
 
 int main() {
 
@@ -13,23 +16,65 @@ int main() {
     myFile.open("image.ppm");
 
     // Image
+    float aspect_ratio = 16.0/9.0;
+    int img_width  = 400;
+    int img_height = img_width / aspect_ratio; // Calculate img_height based on width to maintain aspect ratio
+    if (img_height < 1)
+        img_height = 1; 
 
-    int img_width  = 256;
-    int img_height = 256;
+    std::cout << "Img Dimensions: " << img_width << "x" << img_height << std::endl;
+
+    // Camera setup
+    float focal_length = 1.0;
+    vec3 camera_center(0,0,0);
+
+    // Viewport setup
+    float viewport_height = 2.0;
+    float viewport_width = viewport_height * ((float)img_width / img_height);
+
+    std::cout << "Viewport Dimensions: " << viewport_width << "x" << viewport_height << std::endl;
+
+    // Vectors to go along the edges of the viewport
+    vec3 viewport_u = vec3(viewport_width, 0, 0); // Goes from left to right
+    vec3 viewport_v = vec3(0, -viewport_height, 0); // Goes from top to bottom (must be negative)
+    
+    std::cout << "Viewport edge vectors (u,v): " << std::endl;
+    viewport_u.print();
+    viewport_v.print();
+
+    // Calculate the spacing between each pixel
+    vec3 pixel_du = multiply(viewport_u, (float)1/img_width); 
+    vec3 pixel_dv = multiply(viewport_v, (float)1/img_height);
+
+    std::cout << "Pixel spacing (stride) (u,v)" << std::endl;
+    pixel_du.print();
+    pixel_dv.print();
+
+    // Upper-left hand pixel of viewport
+    // camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2
+    vec3 viewport_upper_left = subtract(subtract(subtract(camera_center, vec3(0, 0, focal_length)), multiply(viewport_u, 0.5)), multiply(viewport_v, 0.5));
+
+    // viewport_upper_left + 0.5 * (pixel_du + pixel_dv)
+    vec3 first_pix_loc = add(viewport_upper_left, multiply(add(pixel_du, pixel_dv), 0.5));
+
+    std::cout << "Upper left corner: ";
+    viewport_upper_left.print();
+    std::cout << "First pixel location: ";
+    first_pix_loc.print();
 
     // Render
-
     myFile << "P3\n" << img_width << ' ' << img_height << "\n255\n";
 
     for (int i = 0; i < img_height; i++) {
         for (int j = 0; j < img_width; j++) {
 
-            // Create a vector that holds the pixel color
-            vec3 pixel_color(double(j) / (img_width-1),
-                             double(i) / (img_height-1),
-                             double(i) / (img_height-1));
+            // first_pix_loc + (i * pixel_du) + (j * pixel_dv)
+            vec3 pixel_center = add(add(first_pix_loc, multiply(pixel_du, j)), multiply(pixel_dv, i));
+            vec3 ray_dir = subtract(pixel_center, camera_center);
+            ray r(camera_center, ray_dir);
 
             // Send pixel color to file for output
+            vec3 pixel_color = ray_color(r);
             writeColor(myFile, pixel_color);
 
         }
@@ -50,10 +95,19 @@ void writeColor(std::ofstream& outputFile, const vec3& pixel_color) {
     float b = pixel_color.getZ();
 
     // Translate all the [0,1] values to RGB [0,255]
-    int rByte = int(255.999 * r);
-    int gByte = int(255.999 * g);
-    int bByte = int(255.999 * b);
+    int rByte = (int)255.999 * r;
+    int gByte = (int)255.999 * g;
+    int bByte = (int)255.999 * b;
 
     outputFile << rByte << ' ' << gByte << ' ' << bByte << "\n";
 
+}
+
+vec3 ray_color(const ray &r)
+{
+    // Fade from color2 to color1 in the vertical y direction
+    float alpha = 0.5 * (r.getDirection().getY() + 1.0);
+    vec3 color1(1, 1, 1);
+    vec3 color2(0.3, 0.5, 1.0);
+    return add(multiply(color1, (1.0 - alpha)), multiply(color2, alpha));
 }
