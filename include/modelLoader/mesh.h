96fc829c3742ce8d0176ac17c53e4937ef817a32
RLucas1102 <robenoltl@gmail.com>
2025-04-07 22:43:59 -0400

    Added model loader classes

---
+++ include/modelLoader/mesh.h
@@ -0,0 +1,85 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+
+// Using a struct to represent vertices is convenient for later as it translates 1-to-1 with OpenGL
+struct Vertex
+{
+    glm::vec3 position;
+};
+
+
+class Mesh {
+
+    private:
+        // Create vectors to hold vertex data and index data, respectively
+        std::vector<Vertex> _vertices;
+        std::vector<unsigned int> _indices;
+
+        // Creating buffers and vertex array
+        unsigned int _VBO, _EBO;
+
+        void setupMesh() {
+
+            // Generate IDs for buffers and vertex arrays
+            glGenBuffers(1, &_VBO);
+            glGenBuffers(1, &_EBO);
+            glGenVertexArrays(1, &_VAO);
+
+            // Bind a vertex array; all subsequent VBOs and attributes will be bound to this object
+            glBindVertexArray(_VAO);
+
+            // Bind array buffer type and then vertex data
+            glBindBuffer(GL_ARRAY_BUFFER, _VBO);
+            glBufferData(GL_ARRAY_BUFFER, _vertices.size() * sizeof(Vertex), &_vertices[0], GL_STATIC_DRAW);
+
+            // Bind element buffer type and then index data
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _EBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, _indices.size() * sizeof(unsigned int), &_indices[0], GL_STATIC_DRAW);
+
+            // Position attribute
+            // Index = 0
+            // Number of components = 3
+            // Type = GL_FLOAT
+            // Normalized = False
+            // Stride: 3 * 4 = 12 (Each vertex is 12 bytes apart)
+            // Offset: offsetof() should return 0
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+            glEnableVertexAttribArray(0); // Enable position attribute at position 0
+
+            // Unbind current VAO
+            glBindVertexArray(0);
+
+        }
+
+    public:
+
+        unsigned int _VAO;
+
+        // Default constructor
+        Mesh(const std::vector<Vertex> &vertices, const std::vector<unsigned int> &indices) : _vertices(vertices), _indices(indices) {
+
+            // Load data onto GPU
+            setupMesh();
+
+        };
+
+        void draw() {
+
+            // Bind VAO and draw mesh
+            glBindVertexArray(_VAO);
+            glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(_indices.size()), GL_UNSIGNED_INT, 0);
+            glBindVertexArray(0);
+
+        }
+
+
+
+    
+};
+
+#endif
---
+++ include/modelLoader/model.h
@@ -0,0 +1,84 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <modelLoader/mesh.h>
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+#include <glm/glm.hpp>
+#include <vector>
+#include <iostream>
+
+class Model {
+    private:
+        std::vector<Mesh> _meshes;
+
+        void loadModel(std::string path) {
+            Assimp::Importer importer;
+            const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate);
+
+            if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+                std::cerr << "ERROR::ASSIMP::" << importer.GetErrorString() << std::endl;
+            }
+
+            processNode(scene->mRootNode, scene);
+
+        }
+        void processNode(aiNode* node, const aiScene* scene) {
+
+            for (int i = 0; i < node->mNumMeshes; i++) {
+                aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+                _meshes.push_back(processMesh(mesh, scene));
+            }
+
+            for (int i = 0; i < node->mNumChildren; i++) {
+                processNode(node->mChildren[i], scene);
+            }
+            
+        }
+
+        Mesh processMesh(aiMesh* mesh, const aiScene* scene) {
+            std::vector<Vertex> vertices;
+            std::vector<unsigned int> indices;
+
+            for (int i = 0; i < mesh->mNumVertices; i++) {
+
+                Vertex vertex;
+                glm::vec3 vector;
+                vector.x = mesh->mVertices[i].x;
+                vector.y = mesh->mVertices[i].y;
+                vector.z = mesh->mVertices[i].z;
+                vertex.position = vector;
+                vertices.push_back(vertex);
+
+            }
+
+            for (int i = 0; i < mesh->mNumFaces; i++) {
+                aiFace face = mesh->mFaces[i];
+                for(int j = 0; j < face.mNumIndices; j++) {
+                    indices.push_back(face.mIndices[j]);
+                }
+            }
+            
+            return Mesh(vertices, indices);
+
+        }
+
+    public:
+        Model(const char* path) {
+
+            loadModel(path);
+
+        }
+
+        void Draw() {
+            for (int i = 0; i < _meshes.size(); i++) {
+                _meshes[i].draw();
+            } 
+        }
+
+
+};
+
+
+#endif
